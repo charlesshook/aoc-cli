2ffd427aed4f7367c1614e41c569c8b4d41e849a
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2024-11-27 11:12:55 -0700

    Gets the AOC prompt and saves it to a .md file

---
+++ cmd/prompt.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	helpers "github.com/charlesshook/aoc-cli/utils"
+	"github.com/spf13/cobra"
+)
+
+var promptCmd = &cobra.Command{
+	Use:   "prompt",
+	Short: "Gets AOC prompt.",
+	Long:  "Gets the Advent of Code prompt for a given day and year",
+	Run:   getPrompt,
+}
+
+func init() {
+	rootCmd.AddCommand(promptCmd)
+
+	promptCmd.Flags().IntP("day", "d", time.Now().Day(), "Day to grab the input file.")
+	promptCmd.Flags().IntP("year", "y", time.Now().Year(), "Year to grab the input file.")
+	promptCmd.Flags().StringP("cookie", "c", "", "Cookie used to make the request to Advent of Code.")
+}
+
+func getPrompt(cmd *cobra.Command, args []string) {
+	day, _ := cmd.Flags().GetInt("day")
+	year, _ := cmd.Flags().GetInt("year")
+	cookie, _ := cmd.Flags().GetString("cookie")
+
+	if day < 1 || day > 25 {
+		fmt.Println("Day must be between 1 and 25!")
+		return
+	}
+
+	if year < 2015 || year > time.Now().Year() {
+		fmt.Printf("Year cannot be before 2015 or after %d.\n", time.Now().Year())
+		return
+	}
+
+	if cookie == "" {
+		fmt.Println("Cookie cannot be left empty")
+		return
+	}
+
+	url := fmt.Sprintf("https://adventofcode.com/%d/day/%d", year, day)
+	data, _ := helpers.GetDataFromUrlWithCookie(url, cookie)
+
+	cwd, err := os.Getwd()
+
+	if err != nil {
+		fmt.Println("Could not get cwd")
+		return
+	}
+
+	helpers.WriteToFile(fmt.Sprintf("%s/%d/%d/PROMPT.md", cwd, year, day), data)
+}
